267ae651ea320d7ff34bc4eac02de193d05dff5b
Isabel Mill <isabelmavd2@gmail.com>
2022-05-16 18:00:57 +0300

    g

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+const path = require('path')
+
 module.exports = {
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/free-games/'
     : '/'
