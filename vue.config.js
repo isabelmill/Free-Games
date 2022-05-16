2a8c5bd9e40c6f103ec5680944267de259d7d2f6
Isabel Mill <isabelmavd2@gmail.com>
2022-05-16 17:34:20 +0300

    j

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === "production" ? "/free-games/" : "/",
-  })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/free-games/'
+    : '/'
+}
