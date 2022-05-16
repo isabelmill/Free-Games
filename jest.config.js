afd529c2c679b5e53dd96d2ed80c76489a300c84
Isabel Mill <isabelmavd2@gmail.com>
2022-05-16 15:31:34 +0300

    d

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
-  publicPath: process.env.NODE_ENV === "production" ? "/free-games/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/free-games/" : "/",
 }
